9e2f2e17d9c1577b3736adc1ccf2020e3e77cd76
Eric Brousseau <emb417@gmail.com>
2018-02-05 01:20:17 -0800

    standardized on rgba for colors; added config params

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 /**
  * whatIsTheMatrix accepts a configOverride object...
  * @param {Object} configOverride - see parameters below...
- * @param {string} canvasId - defaults to 'what-is-the-matrix'
  * @param {number} canvasHeight - defaults to window.innerHeight
+ * @param {string} canvasId - defaults to 'what-is-the-matrix'
  * @param {number} canvasWidth - defaults to window.innerWidth
- * @param {number} textSize - defaults to window.outerWidth / 100
- * @param {number} textDecay - defaults to 0.002
  * @param {Array} charset - default size 42, starting at char code 65393
- * @param {number} speed - default is 50ms
+ * @param {number} fontAlpha - font transparency, defaults to 0.8
+ * @param {string} fontColor - rgb, defaults to random
+ * @param {string} fontFace - defaults to arial
+ * @param {number} fontSize - pt, defaults to window.outerWidth / 100
+ * @param {Array} fontSizeOffsets - randomly adds to fontSize, defaults to [0]
+ * @param {number} fontSpeed - ms, default is 100ms
+ * @param {string} themeColor - rgb, default is random
+ * @param {number} themeAlpha - font decay, defaults to 0.2
  */
+
+const randomFloor = ( length = 0 ) => Math.floor( Math.random() * length );
+
+const randomRGBColor = () => Array.from( new Array(3), ( x, i ) => randomFloor(256) ).join(',');
+
 const whatIsTheMatrix = ( configOverride ) => {
 
   // config object, spreads configOverride after defaults are set
   const config = {
-    'canvasId': 'what-is-the-matrix',
     'canvasHeight': window.innerHeight,
+    'canvasId': 'what-is-the-matrix',
     'canvasWidth': window.innerWidth,
-    'textSize': Math.floor( window.outerWidth / 100 ),
-    'textDecay': 0.002,
     'charset': Array.from( new Array(42), (x, i) => String.fromCharCode(i + 65393) ),
-    'speed': 50,
+    'fontAlpha': 0.8,
+    'fontColor': randomRGBColor(),
+    'fontFace': 'arial',
+    'fontSize': Math.floor( window.outerWidth / 100 ),
+    'fontSizeOffsets': [0],
+    'fontSpeed': 100,
+    'themeAlpha': 0.2,
+    'themeColor': randomRGBColor(),
     ...configOverride
   }
 
@@ -27,7 +42,7 @@ const whatIsTheMatrix = ( configOverride ) => {
   const cnvs = document.createElement('canvas');
     // set an id for custom styling
     cnvs.setAttribute('id', config.canvasId);
-    cnvs.setAttribute('style', 'background: black');
+    cnvs.setAttribute('style', `background: rgb( ${ config.themeColor } )`);
 
   // append the canvas to the html body
   document.body.appendChild(cnvs);
@@ -38,37 +53,37 @@ const whatIsTheMatrix = ( configOverride ) => {
   // using a 2d context for rendering in the canvas
   const ctx = cnvs.getContext('2d');
 
-  // determine possible y positions based on canvas width
-  const yPositions = Array( Math.round( config.canvasWidth / config.textSize ) ).join(0).split('');
+  // create array of columns based on canvas width
+  const columns = Array( Math.round( config.canvasWidth / config.fontSize ) ).join(0).split('');
 
   // draw a character every 50ms
   setInterval( () => {
-    // fade to black
-    ctx.fillStyle = `rgba( 0, 0, 0, ${( config.textSize * config.textDecay )} )`;
+    // set background color
+    ctx.fillStyle = `rgba( ${ config.themeColor }, ${ config.themeAlpha } )`;
     // create rectangle
     ctx.fillRect( 0, 0, config.canvasWidth, config.canvasHeight );
-    // use matrix green
-    ctx.fillStyle = '#0F0';
-    // set text size with arial font
-    ctx.font = `${ config.textSize }pt arial`;
 
-    // what is the matrix, chaotic randomness (read: this is the maths)
-    yPositions.map( ( y, index, text, x ) => {
-      // grab a character from the array
-      text = config.charset[ Math.floor( Math.random() * config.charset.length ) ];
-      // select a column
-      x = index * config.textSize;
-      // draw a character
-      ctx.fillText( text, x, y );
-      // if next position is off the screen, get new random starting position
-      // otherwise draw next character below previous character
+    // what is the matrix, chaotic beauty (read: this is the maths)
+    columns.map( ( y, index, character, x ) => {
+      // randomly decide to get new random starting position
+      // OR draw next character below previous character
       if(y > ( Math.random() * 1e4) ) {
-        const randomStartPosition = Math.floor( Math.random() * 10 ) * Math.floor( Math.random() * 10 );
-        yPositions[index] = randomStartPosition * config.textSize;
+        const randomStartPosition = randomFloor(10) * randomFloor(10);
+        columns[index] = randomStartPosition * config.fontSize;
       }
       else {
-        yPositions[index] = Math.abs( y + config.textSize );
+        columns[index] = Math.abs( y + config.fontSize );
       }
+      // set font color
+      ctx.fillStyle = `rgba( ${ config.fontColor }, ${ config.fontAlpha } ) `;
+      // set font size and font face
+      ctx.font = `${ config.fontSize + config.fontSizeOffsets[ randomFloor(config.fontSizeOffsets.length) ] }pt ${ config.fontFace }`;
+      // grab a character from the array
+      character = config.charset[ randomFloor(config.charset.length) ];
+      // select a column
+      x = index * config.fontSize;
+      // draw a character
+      ctx.fillText( character, x, y );
     });
-  }, config.speed );
+  }, config.fontSpeed );
 };
